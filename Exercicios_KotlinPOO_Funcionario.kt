6180bc3e9cf0428f4195505ff05bd32d7597b391
Victor Saade Damião <victorsaade@outlook.com>
2022-05-24 23:45:03 -0300

    Exercicios Kotlin POO pt4

---
+++ Exercicios_KotlinPOO_Funcionario.kt
@@ -0,0 +1,16 @@
+fun main(){
+    //4) Crie uma classe funcionário e apresente os atributos e métodos
+    //referentes esta classe, em seguida crie um objeto funcionário, defina as
+    //instancias deste objeto e apresente as informações deste objeto no
+    //console.
+
+    var funcionario = Funcionario(
+        "Victor",
+        "Desenvolvedor Mobile Jr",
+        "TI",
+        6000.00,
+        "Natália"
+    )
+
+    funcionario.desempenho(100)
+}
--- Funcionario.kt
+++ Funcionario.kt
@@ -1,2 +1,21 @@
-class Funcionario {
+class Funcionario (
+    var nome: String,
+    var cargo: String,
+    var departamento: String,
+    var salario: Double,
+    var gerenteArea: String
+        ){
+
+    //Método - avaliação de desempenho
+
+
+    fun desempenho(nota: Int){
+        if(nota >= 80){
+            println("Desempenho excelente: Elegivel a promoção.")
+        }else if(nota > 60){
+            println("Desempenho aceitável: precisa melhorar.")
+        }else{
+            println("Feedback alerta.")
+        }
+    }
 }
