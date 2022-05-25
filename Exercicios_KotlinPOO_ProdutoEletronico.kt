518c1018c5ce88398841635b40c7b53cbaaf4fc1
Victor Saade Damião <victorsaade@outlook.com>
2022-05-24 23:33:41 -0300

    Exercicios Kotlin POO pt3

---
+++ Exercicios_KotlinPOO_ProdutoEletronico.kt
@@ -0,0 +1,18 @@
+fun main(){
+
+    //3) Crie uma classe produto eletrônico e apresente os atributos e métodos
+    //referentes esta classe, em seguida crie um objeto produto eletrônico,
+    //defina as instancias deste objeto e apresente as informações deste objeto
+    //no console.
+
+    val celular = Produto_Eletronico(
+        "Digital",
+        "Apple",
+        "Iphone X",
+        30.00,
+        3000.00
+    )
+
+    celular.ligaDesliga()
+    celular.ligaDesliga()
+}
--- Produto_Eletrônico.kt
+++ Produto_Eletrônico.kt
@@ -1,2 +1,19 @@
-class Produto_Eletronico {
+class Produto_Eletronico (
+    var tipo: String,
+    var marca: String,
+    var modelo: String,
+    var valorCusto: Double,
+    var valorMercado: Double
+        ){
+
+    var ligado = false
+
+    fun ligaDesliga(){
+        if(ligado){
+            println("Off")
+        }else{
+            ligado = true
+            println("On")
+        }
+    }
 }
